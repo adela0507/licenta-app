8dae2335869ed2d38027507da945ea215998bd19
adela miclea <adelamiclea709@yahoo.com>
2023-02-27 13:47:35 +0200

    Licenta1

--- src/routes/Status.js
+++ src/routes/Status.js
@@ -1,8 +1,13 @@
+
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 function Status(){
 return(
-    <>
+    <div>
+        <Navbar/>
     <h1>This is status</h1>
-    </>
+    <Footer/>
+    </div>
 );
 }
 export default Status;
